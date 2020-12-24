a5aecd52c61dc71e54b07045890b8a22a459a15f
HaSaN <mahamudul951@gmail.com>
2020-12-25 01:19:20 +0600

    route and parametar view

---
+++ resources/views/mydashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My First Web Project</title>
+
+<body>
+<h1 style="color:green">This is dashboard</h1>
+
+My name is hasan.
+
+name: {{$name}}
+</body>
+</head>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/my',function(){
+return view('mydashboard',['name'=>"My name is babu"]);
+});
