55d18dcdcca6c9567306ae7390968ba9da89c4a7
HaSaN <mahamudul951@gmail.com>
2020-12-25 23:18:04 +0600

    url('/')

--- resources/views/myabout.blade.php
+++ resources/views/myabout.blade.php
@@ -31,7 +31,7 @@
     <body class="antialiased">
     <nav class="navbar navbar-expand-lg navbar-info bg-info">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Home</a>
+    <a class="navbar-brand" href="{{url('/')}}">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
