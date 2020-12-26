0b2e0cac13545c9e5c0fed31e1dfdd175f66e781
HaSaN <mahamudul951@gmail.com>
2020-12-26 12:18:15 +0600

    seeder Model Class

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\Student;
 
 class StudentSeeder extends Seeder
 {
@@ -16,37 +17,23 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Students')->insert([
-            [
-                'name' => 'hasan',
-                'email' => 'hasan@gmail.com',
-                'phone' => '01956565756',
-                'address' => 'badda,dhaka',
-            ],
-            [
-                'name' => 'babu',
-                'email' => 'babu@gmail.com',
-                'phone' => '01956569556',
-                'address' => 'badda,dhaka',
-            ],
-            [
-                'name' => 'mamun',
-                'email' => 'mamun@gmail.com',
-                'phone' => '01923565756',
-                'address' => 'uttara,dhaka',
-            ],
-            [
-                'name' => 'asif',
-                'email' => 'asif@gmail.com',
-                'phone' => '01956925756',
-                'address' => 'badda,dhaka',
-            ],
-            [
-                'name' => 'sojib',
-                'email' => 'sojib@gmail.com',
-                'phone' => '01316565756',
-                'address' => 'tongi,dhaka',
-            ],
-        ]);
+        // DB::table('Students')->insert([
+        //     [
+        //         'name' => 'hasan',
+        //         'email' => 'hasan@gmail.com',
+        //         'phone' => '01956565756',
+        //         'address' => 'badda,dhaka',
+        //     ],
+           
+        // ]);
+
+        for ($i=0;$i<8;$i++){
+            Student::create([
+                'name' => Str::random(10),
+                 'email' => Str::random(10).'@gmail.com',
+                 'phone' => Str::random(11),
+                 'address' => Str::random(),
+            ]);
+        }
     }
 }
