b2e84da558566d8dff4b4c800b4e48dde51f9730
HaSaN <mahamudul951@gmail.com>
2020-12-27 11:13:05 +0600

    home

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -57,6 +57,9 @@
 
 <div class="container">
   @yield('content')
+  <div class="jumbotron">
+      <h1> Home </h1>
+   </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
