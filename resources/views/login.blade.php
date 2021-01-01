63a5fb67baba3581b2e724d14f53ec7f6d8057f8
HaSaN <mahamudul951@gmail.com>
2021-01-01 23:46:20 +0600

    Create User

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -59,15 +59,15 @@
 
 					<div class="text-center p-t-12">
 						<span class="txt1">
-							Create your 
+							Forgot
 						</span>
 						<a class="txt2" href="{{url('/')}}">
-							User?
+							Username / Password?
 						</a>
 					</div>
 
 					<div class="text-center p-t-136">
-						<a class="txt2" href="#">
+						<a class="txt2" href="{{url('/')}}">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
